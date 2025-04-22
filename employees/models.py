9226c5723ea1223770cd65204bf8bb81a3bc9d27
yusaiOfficial <yusai.masamoto@hit-inc.jp>
2025-04-22 11:20:29 +0900

    employees.app の作成とモデルのマイグレーション

---
+++ employees/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
