f14275d70469367d2a63dd263826bd3e2b40f0b6
yusaiOfficial <yusai.masamoto@hit-inc.jp>
2025-04-23 10:29:50 +0900

    ログイン・ログアウト機能実装

--- daily_report_system/settings.py
+++ daily_report_system/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'employees.apps.EmployeesConfig',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
