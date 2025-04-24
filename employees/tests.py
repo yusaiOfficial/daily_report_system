0404eb4ff6a845bef8741fa74ad43737ce91c5dc
yusaiOfficial <yusai.masamoto@hit-inc.jp>
2025-04-24 11:19:10 +0900

    models test OK

--- employees/tests.py
+++ employees/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
 from employees.models import Employee
+from django.contrib.auth.models import User
 
 class EmployeeModelTest(TestCase):
-    def setup(self):
+    def setUp(self):
         self.user = User.objects.create_user(username="testuser", password="password")
+
 # self は自分自身に操作を加えたいときに使うもの
+
     def test_employee_str_with_user(self):
         employee = Employee.objects.create(
             name="Alice",
@@ -12,11 +15,11 @@ class EmployeeModelTest(TestCase):
             department="HR",
             user=self.user
         )
-        self.assertEqual(str(employee), "Alice (testuser)")
+        self.assertEqual(str(employee), "Alice  (testuser)")
     def test_employee_str_without_user(self):
         employee = Employee.objects.create(
             name="Bob",
             email="bob@example.com",
             department="Finance"
         )
-        self.assertEqual(str(employee), "Bob (No User)")
+        self.assertEqual(str(employee), "Bob  (No User)")
